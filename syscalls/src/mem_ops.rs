924666a68b8371b34a7203d58cc342e4ccc9e95f
dkcumming <daniel.cumming@runtimeverification.com>
2025-08-13 14:32:14 +1000

    Removed all extra things except for `SyscallMemCpy`

--- syscalls/src/mem_ops.rs
+++ syscalls/src/mem_ops.rs
@@ -1,4 +1,4 @@
-use {super::*, crate::translate_mut};
+use super::{InvokeContext, declare_builtin_function, Error, MemoryMapping, SyscallError, consume_compute_meter, translate_mut, translate_slice, translate_slice_mut, touch_slice_mut};
 
 fn mem_op_consume(invoke_context: &mut InvokeContext, n: u64) -> Result<(), Error> {
     let compute_cost = invoke_context.get_execution_cost();
@@ -46,94 +46,6 @@ declare_builtin_function!(
     }
 );
 
-declare_builtin_function!(
-    /// memmove
-    SyscallMemmove,
-    fn rust(
-        invoke_context: &mut InvokeContext,
-        dst_addr: u64,
-        src_addr: u64,
-        n: u64,
-        _arg4: u64,
-        _arg5: u64,
-        memory_mapping: &mut MemoryMapping,
-    ) -> Result<u64, Error> {
-        mem_op_consume(invoke_context, n)?;
-
-        memmove(invoke_context, dst_addr, src_addr, n, memory_mapping)
-    }
-);
-
-declare_builtin_function!(
-    /// memcmp
-    SyscallMemcmp,
-    fn rust(
-        invoke_context: &mut InvokeContext,
-        s1_addr: u64,
-        s2_addr: u64,
-        n: u64,
-        cmp_result_addr: u64,
-        _arg5: u64,
-        memory_mapping: &mut MemoryMapping,
-    ) -> Result<u64, Error> {
-        mem_op_consume(invoke_context, n)?;
-
-        let s1 = translate_slice::<u8>(
-            memory_mapping,
-            s1_addr,
-            n,
-            invoke_context.get_check_aligned(),
-        )?;
-        let s2 = translate_slice::<u8>(
-            memory_mapping,
-            s2_addr,
-            n,
-            invoke_context.get_check_aligned(),
-        )?;
-
-        debug_assert_eq!(s1.len(), n as usize);
-        debug_assert_eq!(s2.len(), n as usize);
-        // Safety:
-        // memcmp is marked unsafe since it assumes that the inputs are at least
-        // `n` bytes long. `s1` and `s2` are guaranteed to be exactly `n` bytes
-        // long because `translate_slice` would have failed otherwise.
-        let result = unsafe { memcmp(s1, s2, n as usize) };
-
-        translate_mut!(
-            memory_mapping,
-            invoke_context.get_check_aligned(),
-            let cmp_result_ref_mut: &mut i32 = map(cmp_result_addr)?;
-        );
-        *cmp_result_ref_mut = result;
-
-        Ok(0)
-    }
-);
-
-declare_builtin_function!(
-    /// memset
-    SyscallMemset,
-    fn rust(
-        invoke_context: &mut InvokeContext,
-        dst_addr: u64,
-        c: u64,
-        n: u64,
-        _arg4: u64,
-        _arg5: u64,
-        memory_mapping: &mut MemoryMapping,
-    ) -> Result<u64, Error> {
-        mem_op_consume(invoke_context, n)?;
-
-        translate_mut!(
-            memory_mapping,
-            invoke_context.get_check_aligned(),
-            let s: &mut [u8] = map(dst_addr, n)?;
-        );
-        s.fill(c as u8);
-        Ok(0)
-    }
-);
-
 fn memmove(
     invoke_context: &mut InvokeContext,
     dst_addr: u64,
@@ -157,39 +69,4 @@ fn memmove(
 
     unsafe { std::ptr::copy(src_ptr, dst_ptr, n as usize) };
     Ok(0)
-}
-
-// Marked unsafe since it assumes that the slices are at least `n` bytes long.
-unsafe fn memcmp(s1: &[u8], s2: &[u8], n: usize) -> i32 {
-    for i in 0..n {
-        let a = *s1.get_unchecked(i);
-        let b = *s2.get_unchecked(i);
-        if a != b {
-            return (a as i32).saturating_sub(b as i32);
-        };
-    }
-
-    0
-}
-
-#[cfg(test)]
-#[allow(clippy::indexing_slicing)]
-#[allow(clippy::arithmetic_side_effects)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_is_nonoverlapping() {
-        for dst in 0..8 {
-            assert!(is_nonoverlapping(10, 3, dst, 3));
-        }
-        for dst in 8..13 {
-            assert!(!is_nonoverlapping(10, 3, dst, 3));
-        }
-        for dst in 13..20 {
-            assert!(is_nonoverlapping(10, 3, dst, 3));
-        }
-        assert!(is_nonoverlapping::<u8>(255, 3, 254, 1));
-        assert!(!is_nonoverlapping::<u8>(255, 2, 254, 3));
-    }
 }
